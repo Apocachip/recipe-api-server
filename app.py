4c0dc3e806c9dfc10236a0e6b1881a9a75f8697e
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-06-17 16:21:38 +0900

    add

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.recipe import RecipeListResource
+from resources.recipe_info import RecipeResource
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ api = Api(app)
 
 # 경로와 리소스(API 코드)를 연결한다.
 api.add_resource(RecipeListResource, '/recipes')
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
 
 if __name__ == "__main__" :
     app.run()
